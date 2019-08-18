5993ed142f3013918ab310a115c976053c1a5aa9
Makoto Hirose <makolllmako@gmail.com>
2019-08-18 14:37:18 +0000

    feat: provincesの場合にOn/Off機能を追加

--- src/App.tsx
+++ src/App.tsx
@@ -6,21 +6,24 @@ interface OuterProps {}
 interface AppState {
   region: string;
   resolution: string;
+  data: Array<any>;
 }
 
 class App extends React.Component<OuterProps, AppState> {
   state: AppState = {
     region: "world",
-    resolution: ""
+    resolution: "",
+    data: countries
   };
 
   render() {
-    const data = countries;
+    const { data } = this.state;
 
     const options = {
       region: this.state.region,
       resolution: this.state.resolution,
       legend: "none"
+      // enableRegionInteractivity: false
     };
 
     interface x {
@@ -28,14 +31,34 @@ class App extends React.Component<OuterProps, AppState> {
     }
 
     const selectHandler = ({ chartWrapper }: x) => {
+      console.log(chartWrapper.getOptions());
+      const { region } = chartWrapper.getOptions();
       const chart = chartWrapper.getChart();
       const selection = chart.getSelection();
       const index = selection[0].row + 1;
-      const region = data[index][0];
-      console.log(index, region);
+      const target = data[index];
+      const newRegion = target[0].toString();
+      console.log(target);
+
+      if (region === "world" || !newRegion.startsWith(region)) {
+        const newRegion = target[0].toString();
+        this.setState({
+          region: newRegion,
+          resolution: "provinces"
+        });
+        return;
+      }
+
+      target[1] = !target[1];
       this.setState({
-        region: region.toString(),
-        resolution: "provinces"
+        data
+      });
+    };
+
+    const backHandler = () => {
+      this.setState({
+        region: "world",
+        resolution: ""
       });
     };
 
@@ -54,6 +77,7 @@ class App extends React.Component<OuterProps, AppState> {
           options={options}
           data={data}
         />
+        <button onClick={backHandler}>戻る</button>
       </div>
     );
   }
