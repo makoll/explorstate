725c4ad7918a5bcd7dd16e358337b84d75f365a4
Makoto Hirose <makolllmako@gmail.com>
2019-08-25 09:11:51 +0000

    refactor: Objectベースでデータが作られるように

--- src/Map.tsx
+++ src/Map.tsx
@@ -8,25 +8,31 @@ import { decode, encode } from "./util/util";
 interface OuterProps {
   location: H.Location;
 }
+
+interface IMapObject {
+  [key: string]: number;
+}
+
 interface AppState {
   region: string;
   resolution: string;
-  mapArray: Array<Array<string | number>>;
+  mapObject: IMapObject;
   mapParameter: string;
+  mapData: Array<any>;
 }
 
-const getBlankMapArray = () => {
-  let mapObj = Object.keys(countries).reduce(
+const getBlankMapObject = (): IMapObject => {
+  let mapObject = Object.keys(countries).reduce(
     (obj, countryCode) => Object.assign(obj, { [countryCode]: 0 }),
-    { Region: "Data" }
+    {}
   );
   for (const country of Object.keys(countries)) {
-    mapObj = Object.keys(countries[country]).reduce(
+    mapObject = Object.keys(countries[country]).reduce(
       (obj, subdivisionCode) => Object.assign(obj, { [subdivisionCode]: 0 }),
-      mapObj
+      mapObject
     );
   }
-  return Object.entries(mapObj);
+  return mapObject;
 };
 
 interface MapSelectProps {
@@ -37,18 +43,27 @@ class Map extends React.Component<OuterProps, AppState> {
   constructor(props: OuterProps) {
     super(props);
 
-    const mapArray: Array<Array<string | number>> =
-      this.getParamMapArray() || getBlankMapArray();
+    const mapObject: IMapObject =
+      this.getParamMapObject() || getBlankMapObject();
+    const mapData = this.transMapData(mapObject);
     const mapParameter: string = "";
+
     this.state = {
       region: "world",
       resolution: "",
-      mapArray,
-      mapParameter
+      mapObject,
+      mapParameter,
+      mapData
     };
   }
 
-  getParamMapArray = () => {
+  transMapData = (mapObject: IMapObject): Array<any> => {
+    const mapArray = Object.entries(mapObject);
+    const mapData = [["Region", "Data"], ...mapArray];
+    return mapData;
+  };
+
+  getParamMapObject = (): IMapObject | null => {
     const getParams = queryString.parse(this.props.location.search);
     if (!getParams.mapGetParam) {
       return null;
@@ -57,45 +72,49 @@ class Map extends React.Component<OuterProps, AppState> {
     if (typeof cdStr !== "string") {
       cdStr = "";
     }
-    const allData: Array<Array<string | number>> = JSON.parse(decode(cdStr));
-    return allData;
+    const allDataObject: IMapObject = JSON.parse(decode(cdStr));
+    return allDataObject;
   };
 
   selectHandler = ({ chartWrapper }: MapSelectProps) => {
-    const { mapArray } = this.state;
+    const { mapObject, mapData } = this.state;
 
     const chart = chartWrapper.getChart();
     const selection = chart.getSelection();
     const selectedAreaIndex = selection[0].row + 1;
-    const selectedArea = mapArray[selectedAreaIndex];
+    const selectedAreaCode = mapData[selectedAreaIndex][0];
 
     const { region } = chartWrapper.getOptions();
-    const newRegion = selectedArea[0].toString();
-    if (region === "world" || !newRegion.startsWith(region)) {
+    if (region === "world" || !selectedAreaCode.startsWith(region)) {
       this.setState({
-        region: newRegion,
+        region: selectedAreaCode,
         resolution: "provinces"
       });
       return;
     }
 
-    selectedArea[1] = selectedArea[1] ? 0 : 1;
+    mapObject[selectedAreaCode] = mapObject[selectedAreaCode] ? 0 : 100;
 
-    const areaCode = selectedArea[0];
-    let _mapArray = mapArray;
-    if (typeof areaCode === "string") {
-      const countryCode: string = areaCode.split("-")[0];
-      _mapArray = _mapArray.map(areaData => {
-        if (areaData[0] === countryCode) {
-          return [countryCode, 1];
-        }
-        return areaData;
-      });
-    }
+    const countryCode: string = selectedAreaCode.split("-")[0];
+    const mapArray = Object.entries(mapObject);
 
-    const mapParameter = `?mapGetParam=${encode(JSON.stringify(_mapArray))}`;
+    const countriesDistricts = mapArray.filter(areaData =>
+      areaData[0].startsWith(countryCode)
+    );
+    const countriesDistrictsVisited = countriesDistricts.filter(
+      areaData => areaData[1]
+    );
+    const countryScore: number =
+      30 + (countriesDistrictsVisited.length / countriesDistricts.length) * 70;
+    mapObject[countryCode] = countryScore;
+
+    const updatedMapData = this.transMapData(mapObject);
+
+    const mapParameter = `?mapGetParam=${encode(JSON.stringify(mapObject))}`;
+
     this.setState({
-      mapArray: _mapArray,
+      mapObject,
+      mapData: updatedMapData,
       mapParameter
     });
   };
@@ -108,12 +127,14 @@ class Map extends React.Component<OuterProps, AppState> {
   };
 
   render() {
-    const { mapArray } = this.state;
+    const { mapData } = this.state;
 
     const options = {
       region: this.state.region,
       resolution: this.state.resolution,
-      legend: "none"
+      legend: "none",
+      colorAxis: { minValue: 0, colors: ["#C0D0E0", "#CCEDCC"] },
+      backgroundColor: "#90C0E0"
     };
 
     return (
@@ -129,7 +150,7 @@ class Map extends React.Component<OuterProps, AppState> {
           width="100%"
           height="600px"
           options={options}
-          data={mapArray}
+          data={mapData}
         />
         <button onClick={this.backHandler}>戻る</button>
         <div>http://matome-dev:8080/{this.state.mapParameter}</div>
