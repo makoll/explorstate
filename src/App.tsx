f69d98c648fac44ca8e6a1de6121eae883de5205
Makoto Hirose <makolllmako@gmail.com>
2019-09-04 16:20:40 +0000

    feat: GA対応

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,28 @@
 import * as React from "react";
 import { BrowserRouter, Route } from "react-router-dom";
+import * as ReactGA from "react-ga";
+// import * as H from "history";
+// import createBrowserHistory from "history/createBrowserHistory";
 
 import Map from "./Map";
 
-const App = () => (
-  <BrowserRouter>
-    <div>
-      <Route exact path="/" component={Map} />
-    </div>
-  </BrowserRouter>
-);
+ReactGA.initialize("UA-147024416-1");
+interface OuterProps {}
+interface AppState {}
+class App extends React.Component<OuterProps, AppState> {
+  componentDidMount() {
+    const pathname = "/";
+    ReactGA.set({ page: pathname });
+    ReactGA.pageview(pathname);
+  }
+  render() {
+    return (
+      <BrowserRouter>
+        <div>
+          <Route exact path="/" component={Map} />
+        </div>
+      </BrowserRouter>
+    );
+  }
+}
 export default App;
