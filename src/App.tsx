02021097cceb9caab57aed273c086c4670540048
Makoto Hirose <hirose@tritrue.com>
2019-10-24 01:00:14 +0900

    refactor: formatted by prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import * as React from "react";
 import { BrowserRouter, Route } from "react-router-dom";
 import * as ReactGA from "react-ga";
-import { Reset } from 'styled-reset'
+import { Reset } from "styled-reset";
 
 import Map from "./Map";
 
 const trackingId = process.env.NODE_ENV === "production" ? "UA-147024416-1" : "UA-147024416-2";
 ReactGA.initialize(trackingId);
-interface OuterProps { }
-interface AppState { }
+interface OuterProps {}
+interface AppState {}
 class App extends React.Component<OuterProps, AppState> {
   componentDidMount() {
     const pathname = "/";
