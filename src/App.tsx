99a211c1b5ede54dd4ced826808c8fc589cd67a1
Makoto Hirose <hirose.makoto@pathee.com>
2020-01-30 09:30:54 +0900

    refactor: double quate to single quate

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,15 @@
-import React from "react";
-import { BrowserRouter, Route } from "react-router-dom";
-import ReactGA from "react-ga";
-import { Reset } from "styled-reset";
+import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import ReactGA from 'react-ga';
+import { Reset } from 'styled-reset';
 
-import Map from "@/Map";
+import Map from '@/Map';
 
-const trackingId = process.env.NODE_ENV === "production" ? "UA-147024416-1" : "UA-147024416-2";
+const trackingId = process.env.NODE_ENV === 'production' ? 'UA-147024416-1' : 'UA-147024416-2';
 ReactGA.initialize(trackingId);
-interface OuterProps {}
-interface AppState {}
-class App extends React.Component<OuterProps, AppState> {
+class App extends React.Component<{}> {
   componentDidMount() {
-    const pathname = "/";
+    const pathname = '/';
     ReactGA.set({ page: pathname });
     ReactGA.pageview(pathname);
   }
@@ -21,7 +19,7 @@ class App extends React.Component<OuterProps, AppState> {
         <Reset />
         <BrowserRouter>
           <div>
-            <Route exact path="/" component={Map} />
+            <Route exact path='/' component={Map} />
           </div>
         </BrowserRouter>
       </React.Fragment>
