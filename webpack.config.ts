23712fc945a7584656361088c5450ac6c16ac39f
Makoto Hirose <makoto.hirose@vrg.jp>
2020-05-15 12:12:50 +0900

    refactor: format by prettier

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,33 +1,34 @@
-import path from "path";
-import { Configuration } from "webpack";
+import path from 'path';
+
+import { Configuration } from 'webpack';
 
 const config: Configuration = {
-  context: path.join(__dirname, "src"),
-  entry: "./index.tsx",
+  context: path.join(__dirname, 'src'),
+  entry: './index.tsx',
   output: {
-    path: path.join(__dirname, "dist"),
-    filename: "bundle.js",
-    publicPath: "/assets"
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/assets',
   },
   module: {
     rules: [
       {
         test: /\.tsx?$/,
-        use: "awesome-typescript-loader"
-      }
-    ]
+        use: 'awesome-typescript-loader',
+      },
+    ],
   },
   resolve: {
-    extensions: [".ts", ".tsx", ".js", "jsx"],
+    extensions: ['.ts', '.tsx', '.js', 'jsx'],
     alias: {
-      '@': path.resolve(__dirname, 'src')
-    }
+      '@': path.resolve(__dirname, 'src'),
+    },
   },
   devServer: {
-    contentBase: path.join(__dirname, "static"),
-    host: "0.0.0.0",
-    disableHostCheck: true
-  }
+    contentBase: path.join(__dirname, 'static'),
+    host: '0.0.0.0',
+    disableHostCheck: true,
+  },
 };
 
 export default config;
